6d28f28b4464c3d924d117d5ba8d9b821c8a115b
oguzhan <oguzhan2142@hotmail.com>
2021-05-21 21:21:40 +0300

    change color constants for ligher and darker

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 
 abstract class AppColor {
-  static final Color lighter = Color.fromRGBO(63, 63, 71, 1);
-  static final Color darker = Color.fromRGBO(34, 34, 40, 1);
+  static final Color lighter = Color.fromRGBO(34, 34, 40, 1);
+  static final Color darker = Color.fromRGBO(24, 24, 30, 1);
   static final Color white = Color.fromRGBO(232, 235, 240, 1);
   static final Color blue = Color.fromRGBO(47, 104, 222, 1);
 }
