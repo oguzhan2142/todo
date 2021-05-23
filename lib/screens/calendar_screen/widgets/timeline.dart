9c22b5a44c1259fc4cd075cc254fd35fbad57d7b
oguzhan <oguzhan2142@hotmail.com>
2021-05-23 03:08:12 +0300

    working on second widget

--- lib/screens/calendar_screen/widgets/timeline.dart
+++ lib/screens/calendar_screen/widgets/timeline.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:todo/constants/colors.dart';
 import 'package:todo/models/task.dart';
 
 class TimeLine extends StatefulWidget {
@@ -20,87 +21,67 @@ class _TimeLineState extends State<TimeLine> {
     ),
   ];
 
-  late final children = <Widget>[];
-
-  @override
-  void initState() {
-    tasks.sort((a, b) => a.startTime.compareTo(b.startTime));
-    var counter = 0;
-    for (var index = 0; index < 2 * tasks.length + 1; index++) {
-      var task = tasks[counter];
-
-      var diff = task.endTime.difference(task.startTime).inMinutes;
-      if (index % 2 == 0) {
-        var space;
-
-        if (index == 0) {
-          space = task.startTime.minute + task.startTime.hour * 60;
-        } else if (index == 2 * tasks.length) {
-          space = 24 * 60 - (task.endTime.minute + task.endTime.hour * 60);
-        } else {
-          var beforeTask = tasks[counter - 1];
-
-          space = task.startTime.difference(beforeTask.endTime).inMinutes;
-        }
-        print('flex in space $space');
-        children.add(Flexible(
-          flex: space,
-          child: Container(),
-        ));
-      } else {
-        if (counter < tasks.length - 1) {
-          counter++;
-        }
-        print('flex in card $diff');
-        children.add(Flexible(
-          flex: diff,
-          child: Container(
-            margin: EdgeInsets.only(left: 50, right: 20),
-            width: double.infinity,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(10),
-              color: Colors.lime,
-            ),
-          ),
-        ));
-      }
-    }
-    super.initState();
-  }
+  final _key = GlobalKey();
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Stack(
-        children: [
-          Container(
-            width: double.infinity,
-            height: 24 * 60,
-            child: Column(
-              children: children,
-            ),
-          ),
-          Container(
-            height: 24 * 60,
-            color: Colors.blueAccent,
-            child: Column(
+    return GestureDetector(
+      onTap: () {
+        print(_key.currentContext!.size!.height);
+      },
+      child: SingleChildScrollView(
+        child: Stack(
+          children: [
+            Column(
               children: List.generate(
-                24,
-                (index) => Expanded(
-                  flex: 1,
-                  child: Container(
-                    color: Colors.red,
-                    width: double.infinity,
-                    child: Text(
-                      index.toString(),
-                      style: TextStyle(color: Colors.white),
-                    ),
-                  ),
-                ),
+                24 * 2,
+                (index) {
+                  var height = index % 2 == 0 ? 15.0 : 45.0;
+                  if (index % 2 == 0) {
+                    return Container(
+                      width: double.infinity,
+
+                      height: height,
+                      // color: Colors.red,
+                      child: Text(
+                        (index ~/ 2).toString(),
+                        style: TextStyle(
+                          color: AppColor.white,
+                        ),
+                      ),
+                    );
+                  } else {
+                    return Container(
+                      height: height,
+                    );
+                  }
+                },
               ),
             ),
-          ),
-        ],
+            Column(
+              key: _key,
+              children: List.generate(
+                24 * 2,
+                (index) {
+                  var height = index % 2 == 0 ? 15.0 : 45.0;
+                  return Container(
+                    margin: EdgeInsets.symmetric(horizontal: 20),
+                    width: double.infinity,
+                    height: height,
+                    color: Colors.grey,
+                  );
+                },
+              ),
+            ),
+            Container(
+              width: double.infinity,
+              height: 60 * 24,
+              child: Column(
+                // children: List.generate(tasks., (index) => null),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
